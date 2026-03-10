069abdad3703af13ff2d04400d42cdeff3ae5933
B Ramya <144128529+ramyachetty@users.noreply.github.com>
2026-03-10 12:11:27 +0530

    Update PrimePalindrome.java

--- src/com/NumberLopps/PrimePalindrome.java
+++ src/com/NumberLopps/PrimePalindrome.java
@@ -1,7 +1,7 @@
 package com.NumberLopps;
 public class PrimePalindrome {
     public static boolean isPrime(int n,int i ){
-        if (n<2)    return false;
+        if (n>2)    return false;
         if (i*i>n)  return true;
         if (n%i==0) return false;
         return isPrime(n,i+1);
